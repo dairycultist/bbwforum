9f4d0c39c0d1543a91e8c35eb61ab25f7acf0424
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-04-24 22:01:30 -0400

    SQLite3 is not gonna happen rn

--- index.php
+++ index.php
@@ -1,32 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Title of the document</title>
+	<meta charset="UTF-8">
+	<title>bbwforum - an anonymous forum</title>
 	<style>
+		body { font-family: sans-serif; }
 		table { border-collapse: collapse; }
-		th, td { border: 1px solid black; padding: 0.5em 1em; } th { color: white; background: #5895d6; } td { color: grey; }
+		th, td { border: 1px solid black; padding: 0.5em 1em; text-align: center; }
+		th:first-of-type, td:first-of-type { text-align: left; }
+		th { background:rgb(115, 167, 222); }
+		td { color: grey; }
 	</style>
 </head>
 
 <body>
-bbwforum - an anonymous forum
-	
-<table>
-	<tr>
-		<th>Thread</th>
-		<th>Category</th>
-		<th>Posts</th>
-		<th>Images</th>
-		<th>Last Post</th>
-	</tr>
-	<tr>
-		<td><strong><a href>Fat Foxgirls</a></strong><br>basically any foxgirls from anime that are big and chonky. I wanted to see senko so</td>
-		<td>BBW</td>
-		<td>31</td>
-		<td>17</td>
-		<td>Wed Oct 21, 2009 12:09 pm</td>
-	</tr>
+
+<!-- php -S localhost:4444 -t . -->
+
+	<table>
+		<tr>
+			<th>Thread</th>
+			<th>Category</th>
+			<th>Posts</th>
+			<th>Images</th>
+			<th>Last Post</th>
+		</tr>
+		<?php
+
+			echo "
+				<tr>
+					<td><strong><a href>Fat Foxgirls</a></strong><br>basically any foxgirls from anime that are big and chonky. I wanted to see senko so</td>
+					<td>BBW</td>
+					<td>31</td>
+					<td>17</td>
+					<td>Wed Oct 21, 2009 12:09 pm</td>
+				</tr>
+			";
+
+		?>
 	</table>
 </body>
 
