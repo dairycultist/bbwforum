f97e8200e5bdd7ddb4acf09918e05406115e61a8
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-04-24 22:45:52 -0400

    Style a bit

--- index.php
+++ index.php
@@ -4,12 +4,15 @@
 	<meta charset="UTF-8">
 	<title>bbwforum - an anonymous forum</title>
 	<style>
-		body { font-family: sans-serif; }
+		@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+		body { font-family: "Roboto", sans-serif; }
 		table { border-collapse: collapse; }
 		th, td { border: 1px solid black; padding: 0.5em 1em; text-align: center; }
 		th:first-of-type, td:first-of-type { text-align: left; }
 		th { background:rgb(115, 167, 222); }
-		td { color: grey; }
+		td { color: #777; }
+		td a { color: black; font-weight: 700; text-decoration: none; }
+		td a:hover { text-decoration: underline; }
 	</style>
 </head>
 
@@ -27,9 +30,11 @@
 		</tr>
 		<?php
 
+			// grab all thread files in order by date
 			$threads = glob("threads/*", GLOB_NOSORT);
 			array_multisort(array_map('filemtime', $threads), SORT_NUMERIC, SORT_DESC, $threads);
 
+			// display their metadata
 			foreach ($threads as $thread) {
 
 				$meta_and_posts = explode("<END_META>", file_get_contents($thread, false));
@@ -38,8 +43,8 @@
 
 				echo "
 					<tr>
-						<td><strong><a href>$meta[0]</a></strong><br>$meta[1]</td>
-						<td>$meta[2]</td>
+						<td><a href>$meta[0]</a><br>$meta[1]</td>
+						<td><a href>$meta[2]</a></td>
 						<td>$meta[3]</td>
 						<td>$meta[4]</td>
 						<td>$meta[5]</td>
