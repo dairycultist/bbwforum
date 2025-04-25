c6f458c208e95f8fd968978f8b7aa07f897f3660
cultist <75032462+dairycultist@users.noreply.github.com>
2025-04-24 20:40:14 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+	<style>
+		table { border-collapse: collapse; }
+		th, td { border: 1px solid black; padding: 0.5em 1em; } th { color: white; background: #5895d6; } td { color: grey; }
+	</style>
+</head>
+
+<body>
+bbwforum - an anonymous forum
+	
+<table>
+	<tr>
+		<th>Thread</th>
+		<th>Category</th>
+		<th>Posts</th>
+		<th>Images</th>
+		<th>Last Post</th>
+	</tr>
+	<tr>
+		<td><strong><a href>Fat Foxgirls</a></strong><br>basically any foxgirls from anime that are big and chonky. I wanted to see senko so</td>
+		<td>BBW</td>
+		<td>31</td>
+		<td>17</td>
+		<td>Wed Oct 21, 2009 12:09 pm</td>
+	</tr>
+	</table>
+</body>
+
+</html>
