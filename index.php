57b71c5a52e50ab314aca6574b16bea4d6d5da5f
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-04-26 20:34:17 -0400

    Posts look nice

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>bbwforum - an anonymous forum</title>
+	<title>bbwforum</title>
 	<link rel="stylesheet" href="shared.css">
 </head>
 
 <body>
 
 	<!-- php -S localhost:4444 -t . -->
+	
+	<h1>bbwforum - an anonymous forum</h1>
 
 	<table>
 		<tr>
@@ -28,14 +30,14 @@
 			foreach ($threads as $thread) {
 
 				$id = substr($thread, 8);
-				$meta_and_posts = explode("<END_META>", file_get_contents($thread, false));
+				$meta_and_posts = explode("<META_DELIMITER>", file_get_contents($thread, false));
 
 				$meta = explode("\n", $meta_and_posts[0]);
 
 				echo "
 					<tr>
 						<td><a href='thread.php?id=$id'>$meta[0]</a><br>$meta[1]</td>
-						<td><a href>$meta[2]</a></td>
+						<td><a href='?cat=$meta[2]'>$meta[2]</a></td>
 						<td>$meta[3]</td>
 						<td>$meta[4]</td>
 						<td>$meta[5]</td>
--- thread.php
+++ thread.php
@@ -6,25 +6,43 @@
 
 		$thread = "threads/$id";
 
-		$meta_and_posts = explode("<END_META>", file_get_contents($thread, false));
+		$meta_and_posts = explode("<META_DELIMITER>", file_get_contents($thread, false));
 
 		$meta = explode("\n", $meta_and_posts[0]);
-		$posts = str_replace("\n", "<br>", $meta_and_posts[1]);
+		$posts = explode("<POST_DELIMITER>", $meta_and_posts[1]);
 	?>
 
 	<meta charset="UTF-8">
 	<title>bbwforum - <?php echo $meta[0]; ?></title>
 	<link rel="stylesheet" href="shared.css">
+	<style>
+		td { color: black; }
+	</style>
 </head>
 
 <body>
 
 	<!-- php -S localhost:4444 -t . -->
 
-	<?php
+	<table>
+		<?php
+			foreach ($posts as $index => $post) {
 
-		echo $posts;
-	?>
+				$post_parts = explode("<POST_PART>", $post);
+
+				echo "<tr><th>$post_parts[0] #$index</th></tr>";
+				echo "<tr><td>";
+
+				$post_lines = explode("\n", $post_parts[2]);
+
+				foreach ($post_lines as $post_line) {
+					echo "<p>$post_line</p>";
+				}
+				
+				echo "</td></tr>";
+			}
+		?>
+	</table>
 </body>
 
 </html>
